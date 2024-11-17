f2fb79412873b5723912f55c17e75ba0b84373ef
Anurag Pal <apal895@gmail.com>
2024-11-17 12:35:04 +0530

    added ca special req

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -64,7 +64,9 @@ module.exports = ({ env }) => {
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
+          ca: env("DATABASE_SSL_CA")
+            ? require("fs").readFileSync(env("DATABASE_SSL_CA")).toString()
+            : undefined,
           capath: env("DATABASE_SSL_CAPATH", undefined),
           cipher: env("DATABASE_SSL_CIPHER", undefined),
           rejectUnauthorized: false,
