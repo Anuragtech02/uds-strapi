016a47b07fb255643b29e58f5423fc2d80da7729
Anurag Pal <apal895@gmail.com>
2024-12-18 14:05:55 +0530

    removed cache

--- strapi/src/api/currency-rates/controllers/currency-rates.js
+++ strapi/src/api/currency-rates/controllers/currency-rates.js
@@ -3,22 +3,11 @@ module.exports = {
     try {
       const { from = "USD" } = ctx.query;
 
-      // Try to get cached rates
-      const cacheKey = `exchange_rates_${from}`;
-      const cachedRates = await ctx.strapi.cache.get(cacheKey);
-
-      if (cachedRates) {
-        return cachedRates;
-      }
-
       // If not cached, fetch from external API
       const response = await fetch(
         `https://free.ratesdb.com/v1/rates?from=${from}`
       );
       const data = await response.json();
-
-      // Cache the results for 1 hour (3600 seconds)
-      await ctx.strapi.cache.set(cacheKey, data, 3600);
 
       return data;
     } catch (error) {
