ab7831d4e1d63ed4b3161d231d5a9230afdf4f4f
Anurag Pal <apal895@gmail.com>
2025-01-24 14:15:06 +0530

    changes

--- strapi/src/api/search/controllers/search.js
+++ strapi/src/api/search/controllers/search.js
@@ -28,11 +28,10 @@ module.exports = {
         : [];
 
       // Prepare the queries for multiSearch
-      const searchQuery = `"${q}"`; // Wrap the query in quotes for exact phrase matching
       const queries = indices.map((indexUid) => {
         let query = {
           indexUid,
-          q: searchQuery,
+          q,
           limit: parseInt(limit),
           offset: (parseInt(page) - 1) * parseInt(limit),
         };
