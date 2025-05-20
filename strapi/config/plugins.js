08758dfe83ec8ab5e8dcd5b91ea5916738209669
Anurag Pal <apal895@gmail.com>
2025-05-20 14:15:59 +0530

    locale all

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -47,7 +47,7 @@ module.exports = ({ env }) => ({
           },
         },
         entriesQuery: {
-          locale: null,
+          locale: "all",
         },
       },
       blog: {
@@ -84,7 +84,7 @@ module.exports = ({ env }) => ({
           },
         },
         entriesQuery: {
-          locale: null, // Since you only have English content
+          locale: "all", // Since you only have English content
           populate: {
             industries: {
               fields: ["name", "slug"],
@@ -129,7 +129,7 @@ module.exports = ({ env }) => ({
           },
         },
         entriesQuery: {
-          locale: null,
+          locale: "all",
           populate: {
             industries: {
               fields: ["name", "slug"],
